883be9662a0f2f8f758053e88d76dfb6197b2dda
asharma463 <asharma463@myseneca.ca>
2023-06-03 05:14:59 +0000

    EC2 instance is configured in main.tf file

---
+++ modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Ashmit"
+    "App"   = "assignment1"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "assignment1"
+}
